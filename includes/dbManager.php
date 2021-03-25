a04b0c07a9a77bb645d0add4d8f347573cf17d58
Ali <ali.cruz@me.com>
2021-03-24 19:21:12 -0600

    Update dbManager.php

--- includes/dbManager.php
+++ includes/dbManager.php
@@ -2,7 +2,7 @@
 	require_once('database.php');
 
 	// Instancia conexion a la bd
-	$database = new Database('localhost', 'root', 'fR5$55d_tu8w', 'mitsuaki', '3306');
+	$database = new Database('localhost', 'root', '', 'mitsuaki', '3306');
 
 	$linkToDB = $database->connectToDB();
 	
